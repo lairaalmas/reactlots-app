f0519803dc8e7d1cd4a65ca31eacf22b4e20ef53
lairaalmas <lairaalmas@gmail.com>
2026-04-10 00:29:11 -0300

    feature: update lot mock and type

--- api/mocks/lots.ts
+++ api/mocks/lots.ts
@@ -3,7 +3,7 @@ import type { LotDTO } from '../types/lotDTO';
 const mockData: LotDTO = {
   id: 'foundry_cove',
   title: 'Daisy Hovel',
-  description: 'Daisy Hovel is nestled between Crick Cabana and Garden Essence...',
+  description: 'Daisy Hovel is nestled between Crick Cabana and Garden Essence. While the house is small, the size of the lot allows for Sims to add on rooms and make the home larger. It also provides a lot of space for gardening and outdoor objects.',
   price: 16311,
   type: 'residential',
   is_empty_lot: false,
@@ -18,8 +18,8 @@ const mockData: LotDTO = {
     floors: 1,
   },
   image_url: 'https://static.wikia.nocookie.net/sims/images/6/6c/Daisy_Hovel.png/revision/latest?cb=20140913000538',
-  world: 'willow_creek',
-  neighborhood: 'foundry_cove',
+  world: 'Willow Creek',
+  neighborhood: 'Foundry cove',
 };
 
 export const mockGetLotsResponse: { data: LotDTO[] } = {
--- app/types/world.ts
+++ app/types/world.ts
@@ -1,9 +1,5 @@
-export const WORLD_TITLES = ['willow_creek', 'oasis_springs'] as const;
-
-export type WorldTitles = typeof WORLD_TITLES[number];
-
 export type World = {
   id: string;
-  title: WorldTitles;
+  title: string;
   description: string;
 };
