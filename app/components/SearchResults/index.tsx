8cbcdade0a711b1a5e7d54d2fc14fac3c1e29ac3
lairaalmas <lairaalmas@gmail.com>
2026-04-14 16:46:47 -0300

    feature: add empty search info to search results

--- app/components/SearchResults/index.tsx
+++ app/components/SearchResults/index.tsx
@@ -4,12 +4,16 @@ import { Card } from "../Card";
 
 export const SearchResults = () => {
   const { lots } = useLoaderData();
-
+  
   return <div>
-    <ul className="list-unstyled">
-      {lots.map((lot: Lot) => <li className="border-bottom" key={`card-${lot.id}`}>
+    {lots.length > 0 ? <ul className="list-unstyled">{lots.map((lot: Lot) => 
+      <li className="border-bottom" key={`card-${lot.id}`}>
         <Card lot={lot} />
-      </li>)}
-    </ul>
+      </li>)} 
+    </ul> :
+    <div>
+      <p className="text-center mt-3">No lots found</p>
+    </div>}
   </div>;
 }
+
