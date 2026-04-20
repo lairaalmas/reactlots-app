66fb9c2edb0fa1d538de3461499b1759fa9418ca
lairaalmas <lairaalmas@gmail.com>
2026-04-20 17:54:28 -0300

    refactor: comment out mocked info

--- api/mocks/world.ts
+++ api/mocks/world.ts
@@ -1,18 +1,18 @@
-import type { WorldDTO } from '../types/worldDTO';
+// import type { WorldDTO } from '../types/worldDTO';
 
-const mockData: WorldDTO[] = [
-  {
-    id: 'oasis-springs',
-    title: 'Oasis Springs',
-    description:
-      'An oasis nestled in the austere beauty of the western desert, this haven of eclectic culture is fringed by countless soaring palms, and lent vibrant color through impossibly lush landscaping. Oasis Springs offers relaxing retreats from the sun to any traveler who discovers it.',
-  },
-  {
-    id: 'willow-creek',
-    title: 'Willow Creek',
-    description:
-      "Willow Creek is a lush bayou of rolling hills intertwined with marshy riverways. It's home to long-standing reputations and simple lifestyles full of coastal hospitality. Convenient with town comforts, lively neighbors, and a grandeur of property possibilities, Willow Creek is the idyllic place to settle down and settle in.",
-  },
-];
+// const mockData: WorldDTO[] = [
+//   {
+//     id: 'oasis-springs',
+//     title: 'Oasis Springs',
+//     description:
+//       'An oasis nestled in the austere beauty of the western desert, this haven of eclectic culture is fringed by countless soaring palms, and lent vibrant color through impossibly lush landscaping. Oasis Springs offers relaxing retreats from the sun to any traveler who discovers it.',
+//   },
+//   {
+//     id: 'willow-creek',
+//     title: 'Willow Creek',
+//     description:
+//       "Willow Creek is a lush bayou of rolling hills intertwined with marshy riverways. It's home to long-standing reputations and simple lifestyles full of coastal hospitality. Convenient with town comforts, lively neighbors, and a grandeur of property possibilities, Willow Creek is the idyllic place to settle down and settle in.",
+//   },
+// ];
 
-export default mockData;
+// export default mockData;
