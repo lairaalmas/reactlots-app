541a11cc83b7df2717be2ca904aad5d518ae3636
lairaalmas <lairaalmas@gmail.com>
2026-05-02 13:22:33 -0300

    feature: implement favorites page

--- app/pages/FavoritesPage/index.tsx
+++ app/pages/FavoritesPage/index.tsx
@@ -1,15 +1,30 @@
+import { useLoaderData } from 'react-router-dom';
 import { FAVORITE_LOTS_STORAGE_KEY } from '../../utils/constants';
+import { Card } from '../../components/Card';
+import { useFavoriteLots } from '../../hooks/custom/useFavoriteLots';
+import type { Lot } from '../../types/lot';
 
 const FavoritesPage = () => {
-  const favoriteLots = JSON.parse(localStorage.getItem(FAVORITE_LOTS_STORAGE_KEY) || '');
+  const allLots = useLoaderData();
+  const favoriteLotIds = JSON.parse(localStorage.getItem(FAVORITE_LOTS_STORAGE_KEY) || '');
+
+  const { isFavoriteLot, toggleFavoriteLot } = useFavoriteLots();
 
   return (
     <section className="container mt-5">
       <h2>Favorite lots</h2>
-      <ul>
-        {(favoriteLots || []).map((lot: string) => (
-          <li key={lot}>{lot}</li>
-        ))}
+      <header>
+        <p className="fw-bold">{`${favoriteLotIds.length} ${favoriteLotIds.length === 1 ? 'favorite lot' : 'favorite lots'}`}</p>
+      </header>
+      <ul className="list-unstyled">
+        {(favoriteLotIds || []).map((lotId: string) => {
+          const lot = allLots.find((lot: Lot) => lot?.id === lotId);
+          return (
+            <li key={lotId}>
+              <Card lot={lot} isFavoriteLot={isFavoriteLot} toggleFavoriteLot={toggleFavoriteLot} />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
