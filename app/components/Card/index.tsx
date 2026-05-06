a14070ff11e3a72c1953ce4893b217efb1dec1d6
lairaalmas <lairaalmas@gmail.com>
2026-05-05 22:04:37 -0300

    feature: allow user to switch lot list visualization - grid or list

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -4,17 +4,21 @@ import { Money } from '../Money';
 import { Icon } from '../Icon';
 import { ButtonFavorite } from '../ButtonFavorite';
 
-type CardComponent = { lot: Lot; isFavoriteLot: any; toggleFavoriteLot: any };
+export type CardDisplayStyle = 'default' | 'list';
+type CardComponent = {
+  lot: Lot;
+  variant: CardDisplayStyle;
+  isFavoriteLot: any;
+  toggleFavoriteLot: any;
+};
 
-export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) => {
+export const Card = ({ lot, variant = 'default', isFavoriteLot, toggleFavoriteLot }: CardComponent) => {
   if (!lot) return <></>; // TODO: add error card
-
-  const neighborhoodTheme = ''; // `rlt-card--${lot?.neighborhood?.color}`;
 
   const buildingTypes = ['house', 'apartment', 'empty'];
   const buildingTypeClass = buildingTypes.includes(lot?.buildingDetails?.type) ? '' : 'text-danger';
 
-  const availabilityClass = lot?.availability === 'available' ? '' : 'text-danger';
+  // const availabilityClass = lot?.availability === 'available' ? '' : 'text-danger';
   const lotTypeClass = lot?.type === 'residential' ? '' : 'text-danger';
 
   const parseString = (value: string) => {
@@ -30,60 +34,85 @@ export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) =
     return value;
   };
 
+  const transactionType = lot?.transaction?.type;
+
+  const buyDetails = lot?.transaction?.buyDetails;
+  const buyPrice = buyDetails?.price ? <Money value={buyDetails.price} size="inherit" /> : '-';
+
+  const rentDetails = lot?.transaction?.rentDetails;
+  const rentPeriod = rentDetails?.period || 'week';
+  const rentPrice = rentDetails?.rent ? <Money value={rentDetails.rent} size="inherit" /> : '-';
+  const rentDeposit = rentDetails?.deposit ? <Money value={rentDetails.deposit} size="inherit" /> : '-';
+
+  const showLines = false;
+  const renderLines = showLines ? <hr /> : <></>;
+
   const lotImage = () => (
     <Link to={`/lots/${lot?.id}`}>
-      <img className="rlt-search-list__item__thumb" src={lot.imageUrl} alt={`${lot?.title} lot picture`} />
+      <img
+        className={`rlt-search-list__item__thumb ${variant === 'default' ? 'rounded-top-4' : ''}`}
+        src={lot.imageUrl}
+        alt={`${lot?.title} lot picture`}
+      />
     </Link>
   );
 
   const headerInfo = () => (
     <header>
       <div className="d-flex gap-2 justify-content-between">
-        <h3>
+        <h3 className="m-0 p-0">
+          {/* Title */}
           <Link className="link-underline link-underline-opacity-0" to={`/lots/${lot?.id}`}>
             {lot?.title}
           </Link>
         </h3>
-
-        <div>
-          <div className="d-flex align-items-center">
-            {lot?.transaction?.mainPrice > 0 ? <Money value={lot.transaction?.mainPrice} /> : '-'}
-            {(lot?.transaction?.type === 'rent' || lot?.transaction?.type === 'both') && (
-              <span>/{lot?.transaction?.rentDetails?.period || 'week'}</span>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="d-flex">
-        {lot?.neighborhood?.title} ({lot?.world?.title})
-        <span className={neighborhoodTheme}>
-          <Icon name="location_on" />
-        </span>
       </div>
     </header>
   );
 
   const lotInfo = () => (
-    <ul className="list-unstyled mb-3">
+    <ul className="list-unstyled">
+      {(transactionType === 'rent' || transactionType === 'both') && (
+        <>
+          <li>
+            <span className="d-flex align-items-center gap-1">
+              <strong>Deposit:</strong> {rentDeposit}
+            </span>
+          </li>
+          <li className="d-flex align-items-center">
+            <strong className="me-1">Rent:</strong> {rentPrice} <span>/{rentPeriod}</span>
+          </li>
+        </>
+      )}
+      {transactionType === 'buy' && (
+        <li>
+          <span className="d-flex align-items-center gap-1">
+            <strong>Price:</strong> {buyPrice}
+          </span>
+        </li>
+      )}
+      <hr />
+
       {lot?.buildingDetails?.apartmentTitle && (
         <li>
           <strong>Apartment title:</strong>{' '}
           <span className={buildingTypeClass}>{parseString(lot?.buildingDetails?.apartmentTitle)}</span>
         </li>
       )}
+      {renderLines}
+
       <li>
-        <strong>Transaction type:</strong> <span>{parseString(lot?.transaction?.type)}</span>
+        <strong>World:</strong> {lot?.neighborhood?.title}
       </li>
-      {lot?.transaction?.rentDetails?.deposit && (
-        <>
-          <li>
-            <span className="d-flex align-items-center gap-1">
-              <strong>Deposit:</strong>
-              <Money value={lot.transaction?.rentDetails?.deposit || 0} size="inherit" /> (once)
-            </span>
-          </li>
-        </>
-      )}
+      <li>
+        <strong>Neighborhood:</strong> {lot?.world?.title}
+        {/* <Icon name="location_on" /> */}
+      </li>
+      {renderLines}
+
+      <li>
+        <strong>Transaction:</strong> <span>{parseString(lot?.transaction?.type)}</span>
+      </li>
       {/* <li>
         <strong>Availability:</strong> <span className={availabilityClass}>{parseString(lot?.availability)}</span>
       </li> */}
@@ -94,6 +123,7 @@ export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) =
         <strong>Building type:</strong>{' '}
         <span className={buildingTypeClass}>{parseString(lot?.buildingDetails?.type)}</span>
       </li>
+      {renderLines}
     </ul>
   );
 
@@ -101,11 +131,12 @@ export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) =
     <ul className="list-unstyled">
       {parseString(lot?.description) !== '-' && (
         <li>
-          <p className="rlt-search-list__item__description m-0 mb-3">
+          <p className="rlt-search-list__item__description m-0">
             <strong>Description:</strong> {parseString(lot?.description)}
           </p>
         </li>
       )}
+      <hr />
 
       <li>
         <strong>Dimensions:</strong>{' '}
@@ -117,7 +148,7 @@ export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) =
       {lot?.buildingDetails?.type !== 'empty' && (
         <li className="d-flex">
           <strong className="me-2">Caracteristics:</strong>
-          <ul className="list-unstyled d-flex gap-2 fw-semibold">
+          <ul className="list-unstyled d-flex gap-2 fw-semibold flex-wrap">
             <li className="d-flex gap-1 align-items-center">
               <Icon name="hotel" ariaLabel="Berdooms" classNames="sims-gray" />
               <span className="sims-blue-light">{parseNumber(lot?.buildingDetails?.bedrooms)}</span>
@@ -139,19 +170,29 @@ export const Card = ({ lot, isFavoriteLot, toggleFavoriteLot }: CardComponent) =
   );
 
   return (
-    <div className="row my-4">
-      <div className="col-3">{lotImage()}</div>
+    // row + homogen height in grid + fix breaking layout
+    <div className="row m-0 h-100">
+      {/* spacing between cards */}
+      <div className="m-0 p-3 pt-0">
+        {/* card background */}
+        <div className={`${variant === 'default' ? 'sims-bg-muted rounded-4 h-100' : 'row mt-3'}`}>
+          {/* favorite button - position absolute */}
+          <div className="rlt-favorite__container">
+            <ButtonFavorite lotId={lot.id} isFavorite={isFavoriteLot(lot.id)} onToggleFavorite={toggleFavoriteLot} />
+          </div>
 
-      <div className="col-8">
-        {headerInfo()}
+          {/* img */}
+          <div className={`${variant === 'default' ? 'rlt-card__img' : 'col-4 p-0'}`}>{lotImage()}</div>
 
-        {lotInfo()}
+          {/* header + lot + building */}
+          <div className={`p-3 pt-2 ${variant === 'default' ? '' : 'col-8'}`}>
+            {headerInfo()}
 
-        {buildingInfo()}
-      </div>
+            {lotInfo()}
 
-      <div className="col-1">
-        <ButtonFavorite lotId={lot.id} isFavorite={isFavoriteLot(lot.id)} onToggleFavorite={toggleFavoriteLot} />
+            {buildingInfo()}
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/components/SearchResults/index.tsx
+++ app/components/SearchResults/index.tsx
@@ -1,47 +1,101 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import type { Lot } from '../../types/lot';
-import { Card } from '../Card';
+import { Card, type CardDisplayStyle } from '../Card';
 import { useFavoriteLots } from '../../hooks/custom/useFavoriteLots';
+import { Icon } from '../Icon';
+import type { Lot } from '../../types/lot';
 
 export const SearchResults = () => {
   const { lots, filters } = useLoaderData();
+  const { isFavoriteLot, toggleFavoriteLot } = useFavoriteLots();
+  const [lotDisplayStyle, setLotDisplayStyle] = useState<CardDisplayStyle>('default');
 
   const validFilters = Object.entries(filters)?.reduce((acc: string[], n) => {
     const [key, value] = n;
     return value !== '' ? [...acc, key] : acc;
   }, []);
-
   const filterText = validFilters.join(', ');
 
-  const { isFavoriteLot, toggleFavoriteLot } = useFavoriteLots();
+  const changeDisplayStyle = (variant: CardDisplayStyle) => {
+    setLotDisplayStyle(variant);
+  };
+
+  const renderHeader = () => (
+    <header className="mb-3">
+      {validFilters.length > 0 && (
+        <div className="d-flex gap-2">
+          <strong>Filtering by:</strong>
+          <ul className="list-inline">{filterText}</ul>
+        </div>
+      )}
+
+      <div className="d-flex gap-5 justify-content-between align-items-center">
+        <strong>{`${lots.length} ${lots.length === 1 ? 'result' : 'results'}`}</strong>
+
+        <div className="d-flex align-items-center gap-2">
+          <strong>List display style:</strong>
+
+          <div className="btn-group d-flex gap-2" role="group" aria-label="Display style">
+            <input
+              type="radio"
+              className="btn-check"
+              name="listDisplayBtn"
+              id="listDisplayBtnGrid"
+              autoComplete="off"
+              onClick={() => changeDisplayStyle('default')}
+            />
+            <label className="rlt-btn --sm" htmlFor="listDisplayBtnGrid">
+              <Icon name="apps" />
+            </label>
+
+            <input
+              type="radio"
+              className="btn-check"
+              name="listDisplayBtn"
+              id="listDisplayBtnList"
+              autoComplete="off"
+              onClick={() => changeDisplayStyle('list')}
+            />
+            <label className="rlt-btn --sm" htmlFor="listDisplayBtnList">
+              <Icon name="reorder" />
+            </label>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+
+  const renderList = () => (
+    <div>
+      {lots.length > 0 ? (
+        <ul className="list-unstyled row m-0">
+          {lots.map((lot: Lot) => (
+            <li
+              className={`d-flex flex-column p-0 ${lotDisplayStyle === 'default' ? 'col-xxl-3 col-lg-4 col-md-6' : 'border-bottom'}`}
+              key={`card-${lot.id}`}
+            >
+              <Card
+                lot={lot}
+                variant={lotDisplayStyle}
+                isFavoriteLot={isFavoriteLot}
+                toggleFavoriteLot={toggleFavoriteLot}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <p className="text-center mt-3">No lots found</p>
+        </div>
+      )}
+    </div>
+  );
 
   return (
-    <>
-      <header>
-        {validFilters.length > 0 && (
-          <div className="d-flex gap-2">
-            <strong>Filtering by:</strong>
-            <ul className="list-inline">{filterText}</ul>
-          </div>
-        )}
-        <p className="fw-bold">{`${lots.length} ${lots.length === 1 ? 'result' : 'results'}`}</p>
-        {/* TODO: sorting selections */}
-      </header>
-      <div>
-        {lots.length > 0 ? (
-          <ul className="list-unstyled">
-            {lots.map((lot: Lot) => (
-              <li className="border-bottom" key={`card-${lot.id}`}>
-                <Card lot={lot} isFavoriteLot={isFavoriteLot} toggleFavoriteLot={toggleFavoriteLot} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div>
-            <p className="text-center mt-3">No lots found</p>
-          </div>
-        )}
-      </div>
-    </>
+    <div>
+      {renderHeader()}
+
+      {renderList()}
+    </div>
   );
 };
